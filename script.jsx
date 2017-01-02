133b0c53ee29a48d356fe516647c635a9e4a8fa9
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2017-01-02 09:51:30 +0100

    First complete example

--- script.jsx
+++ script.jsx
@@ -1,9 +1,44 @@
 var Button = React.createClass({
+    localHandleClick: function(){
+        this.props.localHandleClick(this.props.increment);
+    },
     render: function(){
         return(
-            <button>Go</button>
+            <button onClick={this.localHandleClick}>+{this.props.increment}</button>
         )
     }
 });
 
-React.render(<Button />, document.getElementById("root"));
+var Result = React.createClass({
+    render: function(){
+        return (
+            <div>{this.props.localCounter}</div>
+        );
+    }
+})
+
+var Main = React.createClass({
+    getInitialState: function(){
+        return{
+            counter: 0
+        }
+    },
+    handleClick: function(increment){
+        this.setState({
+            counter: this.state.counter+increment
+        });
+    },    
+    render: function () {
+        return (
+            <div>
+                <Button localHandleClick={this.handleClick} increment={1} />
+                <Button localHandleClick={this.handleClick} increment={5} />
+                <Button localHandleClick={this.handleClick} increment={10} />
+                <Button localHandleClick={this.handleClick} increment={100} />
+                <Result localCounter={this.state.counter} />
+            </div>
+        )
+    }
+})
+
+React.render(<Main />, document.getElementById("root"));
