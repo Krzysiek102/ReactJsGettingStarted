ecb4f5e628a3c14b26a0c8bb0d85b15e7acbc5c0
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2017-01-09 06:55:30 +0100

    Working starting point of example 2

--- example2/script.jsx
+++ example2/script.jsx
@@ -1,12 +1,25 @@
+import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        };
+    }
+    componentDidMount(){
+        var component = this;
+        $.get("http://api.github.com/users/petehunt", function(data){
+            component.setState(data);
+        });
+    }  
     render() {
         return (
             <div>
-                <img src="https://avatars.githubusercontent.com/u/12782881?v=3" />
-                <h3>Krzysztof Kurek</h3>
+                <img src={this.state.avatar_url} width="80" />
+                <h3>{this.state.name}</h3>
                 <hr />
             </div>
         );
@@ -17,7 +30,7 @@ class Main extends React.Component {
     render() {
         return (
             <div>
-                <Card />
+                <Card login='petehunt' />
             </div>
         )
     }
