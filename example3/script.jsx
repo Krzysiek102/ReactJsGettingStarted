6a8f8aa2506a7c3b7427707e9519e45d5dd7107e
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2017-01-11 08:49:41 +0100

    Add a template for components we want to have

--- example3/script.jsx
+++ example3/script.jsx
@@ -1,16 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Main extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
+class StarsFrame extends React.Component {
     render() {
         return (
-            <div />
-        )
+            <div id="stars-frame">
+                <div className="well">
+                    <span className="glypicon glyphicon-star" />
+                </div>
+            </div>
+        );
     }
 }
 
-ReactDOM.render(<Main />, document.getElementById("root"));
+class ButtonFrame extends React.Component {
+    render() {
+        return (
+            <div>
+                ...
+            </div>
+        );
+    }
+}
+
+class AnswerFrame extends React.Component {
+    render() {
+        return (
+            <div>
+                ...
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div id="game">
+                <h2>Play Nine</h2>
+                <StarsFrame />
+                <ButtonFrame />
+                <AnswerFrame />
+
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
