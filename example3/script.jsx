ee08ce40e2c6f6e90e9afc3fad8b7b7fdfa820ec
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2017-01-11 08:30:47 +0100

    Example 3 starting files

---
+++ example3/script.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div />
+        )
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById("root"));
